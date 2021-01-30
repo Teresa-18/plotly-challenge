105d2a2f8a5f622a87cd6981a8734be6583dad7c
teresa luna <tmluna_18@outlook.com>
2021-01-30 17:44:23 -0600

    updating files and folders

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+var trace1 = {
+    labels: []
+    x: 
+    y: 
+    type: ""
+};
+
+
+var trace2 = 
+
+
+var data = [trace1, trace2]
+
+
+
+var layout = {
+    title: ""
+    xaxis: {title: ""},
+    yaxis: {title: ""}
+};
+
+Plotly.newPlot()
