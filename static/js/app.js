1c3045945b4ccc98c6df474fcda5945fbcf8d51d
teresa luna <tmluna_18@outlook.com>
2021-01-30 18:27:39 -0600

    adding more beginning code to learn what the data consists of

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,42 @@
 
 
 
+function unpack(rows, index) {
+    return rows.map(function(row) {
+        return row[index];
+    });
+}
 
 
 
+d3.json("..samples.json").then(samples) => {
+    console.log(data)
+
+    
+
+    // var data = samples;
+    // //  Create the Traces
+    // var trace1 = {
+    //     labels: [otu_ids]
+    //     x: data.map(row => row.sample_values)
+    //     y: data.map(row => row.otu_ids)
+    //     name: "Top 10 OTUs"
+    //     type: "bar"
+    //     orientation: "h"
+    // };
 
 
-var trace1 = {
-    labels: []
-    x: 
-    y: 
-    type: ""
-};
+    // var trace2 = 
 
 
-var trace2 = 
-
-
-var data = [trace1, trace2]
+    // var data = [trace1, trace2];
 
 
 
-var layout = {
-    title: ""
-    xaxis: {title: ""},
-    yaxis: {title: ""}
-};
+    // var layout = {
+    //     title: ""
+    //     xaxis: {title: ""},
+    //     yaxis: {title: ""}
+    // };
 
-Plotly.newPlot()
+    // Plotly.newPlot()
