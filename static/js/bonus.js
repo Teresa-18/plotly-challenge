a2d23143b57f27a7d1335160d674d72ec54598ca
teresa luna <tmluna_18@outlook.com>
2021-02-01 23:50:16 -0600

    adding some code to try to complete bonus work

---
+++ static/js/bonus.js
@@ -0,0 +1,34 @@
+var data = [
+    {
+      type: "indicator",
+      mode: "gauge+number",
+      value: 420,
+      title: { text: "Washes per Week", font: { size: 24 } },
+      gauge: {
+        axis: { range: [null, 9] },
+        bar: { color: "darkblue" },
+        bgcolor: "white",
+        borderwidth: 2,
+        bordercolor: "gray",
+        steps: [
+          { range: [0, 250], color: "cyan" },
+          { range: [250, 400], color: "royalblue" }
+        ],
+        threshold: {
+          line: { color: "red", width: 4 },
+          thickness: 0.75,
+          value: 490
+        }
+      }
+    }
+  ];
+  
+  var layout = {
+    width: 500,
+    height: 400,
+    margin: { t: 25, r: 25, l: 25, b: 25 },
+    paper_bgcolor: "lavender",
+    font: { color: "darkblue", family: "Arial" }
+  };
+  
+  Plotly.newPlot("gauge", data, layout);
